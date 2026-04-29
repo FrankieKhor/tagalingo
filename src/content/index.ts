78b1d83b7fc9c4760235a3cbc3c4fbf194e0b6b1
FrankieKhor <FrankieKhor@users.noreply.github.com>
2026-04-29 22:19:29 +0100

    Expand curriculum content

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,7 +1,23 @@
-import { achievementSeeds } from "@/content/achievements"
-import { unit01 } from "@/content/units/unit-01"
-import { unit02 } from "@/content/units/unit-02"
-import { unit03 } from "@/content/units/unit-03"
+import { achievementSeeds } from '@/content/achievements'
+import { unit01 } from '@/content/units/unit-01'
+import { unit02 } from '@/content/units/unit-02'
+import { unit03 } from '@/content/units/unit-03'
+import { unit04 } from '@/content/units/unit-04'
+import { unit05 } from '@/content/units/unit-05'
+import { unit06 } from '@/content/units/unit-06'
+import { unit07 } from '@/content/units/unit-07'
+import { unit08 } from '@/content/units/unit-08'
+import { unit09 } from '@/content/units/unit-09'
 
-export const curriculumUnits = [unit01, unit02, unit03]
+export const curriculumUnits = [
+	unit01,
+	unit02,
+	unit03,
+	unit04,
+	unit05,
+	unit06,
+	unit07,
+	unit08,
+	unit09,
+]
 export const achievementCatalog = achievementSeeds
